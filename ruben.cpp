82538729e49cc52fd59a108ea80af5bd0eab88c8
MyBoyDoru <59029310+MyBoyDoru@users.noreply.github.com>
2020-02-13 09:38:21 +0200

    Update ruben.cpp

--- ruben.cpp
+++ ruben.cpp
@@ -5,22 +5,29 @@ ifstream f("lista.in");
 struct nod
 {
     string nume;
+    string autor;
     float nota;
+    int pret;
     nod *urm;
-};
-nod *prim,*ultim;
+}*prim,*ultim;
 
+int N = 0;
 void citire()
 {
     int n;
     string nume;
+    string autor;
     float nota;
-    while(f>>nume>>nota)
+    int pret;
+    while(f>>nume>>nota>>pret>>autor)
     {
+        N++;
         nod *n;
         n = new nod;
         n->nume = nume;
         n->nota = nota;
+        n->pret = pret;
+        n->autor = autor;
         n->urm = NULL;
         if(prim==NULL)
         {
@@ -38,7 +45,7 @@ void afis()
     nod *p=prim;
     while(p != NULL)
     {
-        cout<<p->nume<<" "<<p->nota<<endl;
+        cout<<p->nume<<" "<<p->nota<<" "<<p->pret<<" "<p->autor<<endl;
         p=p->urm;
     }
 }
@@ -59,11 +66,93 @@ void sortare()
     }
 }
 
+float suma()
+{
+    int s=0;
+    nod*p=prim;
+    while(p!=NULL)
+    {
+        s+=p->nota;
+        p=p->urm;
+    }
+    return s;
+}
+
+int pretMax()
+{
+    int Max = 0;
+    vector <int> x;
+    nod *p = prim;
+    while(p != NULL)
+    {
+        x.push_back(p->pret);
+        p = p->urm;
+    }
+    for(auto it:x)
+    {
+        if(it>=Max)
+            Max = it;
+    }
+    return Max;
+}
+
+int pretMin()
+{
+    int Min = INT_MAX;
+    vector <int> x;
+    nod *p = prim;
+    while(p != NULL)
+    {
+        x.push_back(p->pret);
+        p = p->urm;
+    }
+    for(auto it:x)
+    {
+        if(it<=Min)
+            Min = it;
+    }
+    return Min;
+}
+
+void check(vector <string> x,int &f)
+{
+    for(auto it:x)
+    {
+        if(it == name)
+            f++;
+    }
+}
+
+void autor()
+{
+    nod *p = prim;
+    int k[500];
+    memset(k,0,sizeof(k));
+    vector <string> names;
+    while(p!=NULL)
+    {
+        names.push_back(p->autor);
+        p = p->urm;
+    }
+    p = prim;
+    while(p!=NULL)
+    {
+        check()
+        p = p->urm;
+    }
+}
+
 int main()
 {
     citire();
     afis();
     cout<<endl;
     sortare();
+    cout<<endl;
+    cout<<"Media Rating-urilor"<<suma()/N;
+    cout<<endl;
+    cout<<"Cartea ce-a mai scumpa este: "<<pretMax();
+    cout<<endl;
+    cout<<"Cartea ce-a mai ieftina este: "<<pretMin();
     return 0;
 }
